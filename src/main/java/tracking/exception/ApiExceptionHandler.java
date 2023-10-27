d536cf88f0a93fbc30872f49d386d968dc85d74b
ssjeong <ssjeong@gmail.com>
2023-10-27 21:24:22 +0900

    @ControllerAdvice 주석 해제

--- src/main/java/tracking/exception/ApiExceptionHandler.java
+++ src/main/java/tracking/exception/ApiExceptionHandler.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-//@ControllerAdvice
+@ControllerAdvice
 public class ApiExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
