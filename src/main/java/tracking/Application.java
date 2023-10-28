5f860f3cadc903a652759ffd96d4819bbd647dd7
ssjeong <ssjeong@gmail.com>
2023-10-29 02:44:19 +0900

    CI 주석 삭제

--- src/main/java/tracking/Application.java
+++ src/main/java/tracking/Application.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        // travis ci test
         SpringApplication.run(Application.class, args);
     }
 }
